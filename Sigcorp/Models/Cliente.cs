a3e552bbbf772a44f6b0e9a12fb0af1791cdc919
Kesley de Mello <kesley_ksl@hotmail.com>
2019-07-19 10:58:41 -0300

    Primeiros ajustes nas views

--- Sigcorp/Models/Cliente.cs
+++ Sigcorp/Models/Cliente.cs
@@ -1,6 +1,7 @@
 ﻿//cliente
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,10 @@ namespace Sigcorp.Models
     public class Cliente
     {
         public int ClienteID { get; set; }
+        [Display(Name = "Nome")]
         public string NomeCompleto { get; set; }
+        [Display(Name = "CPF")]
+        [Cpf(ErrorMessage = "O valor '{0}' é inválido para CPF")]
         public string Cpf { get; set; }
 
         public Cliente()
--- Sigcorp/Startup.cs
+++ Sigcorp/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.EntityFrameworkCore;
 using Sigcorp.Models;
 using Sigcorp.Data;
 using Sigcorp.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 namespace Sigcorp
 {
     public class Startup
@@ -49,6 +51,15 @@ namespace Sigcorp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var ptBR = new CultureInfo("pt-BR");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(ptBR),
+                SupportedCultures = new List<CultureInfo> { ptBR },
+                SupportedUICultures = new List<CultureInfo> { ptBR}
+
+            };
+            app.UseRequestLocalization(localizationOptions);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
